45cb90e3f033361f378a0c98ea26e09d7d635804
aledyad <aledyad@yandex.ru>
2016-11-23 22:46:58 +0400

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/ask.conf.py /etc/gunicorn.d/ask.conf.py
 sudo ln -s /home/box/web/etc/hello.conf.py /etc/gunicorn.d/hello.conf.py
 sudo /etc/init.d/gunicorn restart
 sudo ﻿/etc/init.d/mysql start﻿
