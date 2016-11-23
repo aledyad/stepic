91b097e58332f2ca217f4d87a2b7307696bc0d27
aledyad <aledyad@yandex.ru>
2016-11-23 22:33:54 +0400

    fix urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from qa import views
+
 urlpatterns = [
 #    url(r'^admin/', admin.site.urls),
-    url(r'^$'),
-    url(r'^login/$'),
-    url(r'^signup/$'),
-    url(r'^question/(\d+)/$'),
-    url(r'^ask/$'),
-    url(r'^popular/$'),
-    url(r'^new/$'),
+    url(r'^$', views.test),
+    url(r'^login/$', views.test),
+    url(r'^signup/$', views.test),
+    url(r'^question/(\d+)/$', views.test),
+    url(r'^ask/$', views.test),
+    url(r'^popular/$', views.test),
+    url(r'^new/$', views.test)
 ]
