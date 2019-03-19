ee261b319db54e2b8bbdf02e5c013e141682bea3
aledyad <aledyad@yandex.ru>
2019-03-19 21:24:39 +0400

    2.6 Обработка форм

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ sudo rm -f /etc/gunicorn.d/*
 sudo ln -s /home/box/web/etc/ask.conf.py /etc/gunicorn.d/ask.conf.py
 sudo ln -s /home/box/web/etc/hello.conf.py /etc/gunicorn.d/hello.conf.py
 sudo /etc/init.d/gunicorn restart
-sudo ﻿/etc/init.d/mysql start﻿
+sudo service mysql start
