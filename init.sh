c997523125ed59b9869e908faa778399fcb8d097
aledyad <aledyad@yandex.ru>
2019-04-04 22:28:43 +0400

    init db

--- init.sh
+++ init.sh
@@ -6,3 +6,6 @@ sudo ln -s /home/box/web/etc/ask.conf.py /etc/gunicorn.d/ask.conf.py
 sudo ln -s /home/box/web/etc/hello.conf.py /etc/gunicorn.d/hello.conf.py
 sudo /etc/init.d/gunicorn restart
 sudo service mysql start
+
+mysql -u root < /home/box/web/etc/create_db.sql
+python /home/box/web/ask/manage.py migrate
